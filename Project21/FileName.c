366b51d69d8bcbff0b83e440ca09b939ed54d6e1
Anastasia-ShA <142487424+Anastasia-ShA@users.noreply.github.com>
2025-02-22 13:17:19 +0300

    Add files via upload

---
+++ Project21/FileName.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <limits.h>
+#include "task1.h"
+
+
+int main() {
+	extern int* a;
+	extern int i, j, m, n;
+	extern int res;
+	extern int mini;
+	extern int* massiveForMinimalnix;
+
+	printf_s("enter kol-vo strok: ");
+	scanf_s("%d", &n);
+	printf_s("enter kol-vo stolcov: ");
+	scanf_s("%d", &m);
+
+	if (n < 0 || m < 0) {
+		n = abs(n);
+		m = abs(m);
+	}
+
+	a = (int*)malloc(n * m * sizeof(int));
+	massiveForMinimalnix = (int*)malloc(n * sizeof(int));
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			printf_s("a[%d][%d]=", i, j);
+			scanf_s("%d", (a + i * m + j));
+
+		}
+	}
+
+	
+
+	printf_s("WER GETTING MASSIVE WITH NUMBERS:\n");
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			printf_s("%5d", *(a + i * m + j));
+		}
+		printf_s("\n");
+	}
+
+	poiskMinimalnogoZnachenia();
+	poiskMaximalnogoIzMinimal();
+	VivodMinimalnixZnacheniyStroki();
+
+	
+
+	printf_s("Rresult maximalniy iz minimalnix -----> %d", res);
+
+	free(a);
+	free(massiveForMinimalnix);
+	getchar();
+	return 0;
+}
